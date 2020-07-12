99908bde534c89af43b82e3f5affb0a41b74eed1
Rik de Vos <rik@rikdevos.com>
2020-07-12 16:14:03 +0200

    update nuxt & tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
-  theme: {},
-  variants: {},
+  theme: {
+    colors: {
+      primary: '#6257b0',
+      secondary: '#75fc93',
+      white: '#ffffff',
+      dark: '#454545',
+      medium: '#d7d7d7',
+      light: '#f5f5f5',
+      secondaryDark: '#344139',
+    },
+    extend: {
+      transitionTimingFunction: {
+        'ease-in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+        'ease-out-expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
+      },
+    },
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+    transitionProperty: ['responsive', 'hover', 'focus'],
+  },
   plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
@@ -16,7 +36,7 @@ module.exports = {
       'layouts/**/*.vue',
       'pages/**/*.vue',
       'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  }
+      'nuxt.config.js',
+    ],
+  },
 }
